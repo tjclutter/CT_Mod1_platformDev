9791653a391195d2457630d3ed230ff27209cfc1
tjclutter <tjclutter@outlook.com>
2024-04-15 16:29:54 -0400

    with comments

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,11 +4,16 @@ import android.os.Bundle
 import android.widget.TextView
 import androidx.activity.ComponentActivity
 
+//mainActivity Class
 class MainActivity : ComponentActivity() {
+    //onCreate function so code executes once created
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //connects activity to layout file
         setContentView(R.layout.layout)
+        //create reference to textbox in layout file
         val textBox = findViewById(R.id.textView) as TextView
+        //edit text of the text box
         textBox.text = "Hello, Android!"
 
     }
